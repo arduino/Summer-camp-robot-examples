c66ecf8903bb1e4683fa23a7469d00459db2e422
Xun Yang <x.yang@arduino.cc>
2014-06-03 15:30:10 +0200

    Added comments for competition content, removed obstacle course example.

--- RobotSummer/Competition/ZGZRobot_IndustryRobot/ZGZRobot_IndustryRobot.ino
+++ RobotSummer/Competition/ZGZRobot_IndustryRobot/ZGZRobot_IndustryRobot.ino
@@ -1,10 +1,28 @@
+/*
+*	Industry Robot
+*	Make a robot that helps manufacturing in factories!
+*	The robot follows a black line, across a few work stations.
+*	Whenever it reaches a workstation(end line), it performs a 
+*	Serials of logo movements to deliver raw materials into the
+*	machine. 
+*	You need to fine tune the logo movement so it both pushes 
+*	the object to the right position, and returns to the track
+*	afterwards. You can also fine tune the line follower 
+*	parameters so it runs faster along the track.
+*
+*	This is an example from the Svante robot summer camp.
+*	zgz.verkstad.cc 
+*	(c) 2014 Arduino Verkstad
+*
+*/
+
 #include <Svante.h>
 #include <EEPROM.h>
 void setup(){
 	robot.begin();
 	delay(1000);
 	robot.lineFollowCalibrate();
-	robot.lineFollowConfig(40,8,10,30);
+	robot.lineFollowConfig(40,8,10,30);//KP, KD, integration time, max speed
 	delay(1000);
 
 }
--- RobotSummer/Competition/ZGZRobot_Sumo/ZGZRobot_Sumo.ino
+++ RobotSummer/Competition/ZGZRobot_Sumo/ZGZRobot_Sumo.ino
@@ -1,32 +1,57 @@
-#include <ArduinoRobot.h>
-#include <Wire.h>
-#include <SPI.h>
+/*
+*	Robot sumo
+*	Enlist your robot in the robot sumo fight! 
+*	You'll need to attach a ultrasonic sensor to DP4, which is used for the right side of the robot,
+*	another to DP8, which is used for the left side of the robot.
+*	Gear up your robot with cardboard/plastic structures so it gains upper-hand. Make some research
+*	and find out what're the best weaponaries people use in professional robot sumo.
+*	There're also parameters you can customize to make your sumo robot work differently. Read the 
+*	descriptions below to find out how they work
+*
+*	This is an example from the Svante robot summer camp.
+*	zgz.verkstad.cc 
+*	(c) 2014 Arduino Verkstad
+*
+*/
 
-#include <Servo.h> 
+#include <Svante.h>
+#include <EEPROM.h>
  
-Servo myServo;  // create servo object to control a servo 
  
-int currentMode;//0:search, 1:track 
+int currentMode;//0:search, 1:track, 2:attack, 3:survive
  
-int servoPos=0;
 int target=0;
 int trackCount=0;
 boolean tracked=false;
-const int trackCountMax=30;
+const int trackCountMax=10;
 const int trackCountThreshold=5;
-const int servoPosMin=30;
-const int servoPosMax=150;
 
-const int valSample_len=5;
-const int detectionRange=30;
+const int pingR=DP4;
+const int pingL=DP8;
+
+const int valSample_len=1;
+
+//****************
+//Customizable variables, read the description about how to tinker your sumo robot
+const int detectionRange=30;  //the robot can see as far as x cm
+const int searchSpeed=60;	//Speed when the robot is in search mode(rotating)
+const int trackSpeed=50;	//Speed when the robot is in track mode(when the target is somewhat off)
+const int attackSpeed=80;	//Speed when the robot is in attack mode(when the target is almost staright forward)
+const int surviveSpeed=60;	//Speed when the robot is in survive mode(when it touches doujo ring and back off)
+const float attackOffset=0.5;	//How big movements do the robot make when in attack mode
+const float trackOffset=0.3;	//How big movements do the robot make when in track mode
+//end of customizable variables
+//****************
+
+int edgeValue=200;	
 
 int error;
 
 void setup() 
 { 
   Serial.begin(9600);
-  Robot.begin();
-  myServo.attach(D1);  // attaches the servo on pin 9 to the servo object 
+  robot.begin();
+
   currentMode=0;
   searchMode();
 } 
@@ -36,12 +61,22 @@ void loop()
   scan();
   if(trackCount>trackCountThreshold)
     tracked=true;
+  else
+    tracked=false;
     
-  Serial.println(tracked);
+  //Serial.println(tracked);
+
+  //Serial.println(currentMode);
+  
   modeManager();
   delay(5);
 }
 void modeManager(){
+  if(onEdge()){
+    currentMode=3;
+    surviveMode();
+    return;
+  }
   if(!tracked){
     if(currentMode!=0){
       currentMode=0;
@@ -57,82 +92,84 @@ void modeManager(){
       trackMode();
     }
   }
-  
 }
 void scan(){
-  int distance=getValAvg();
-  //Serial.println(distance);
-  target=servoPos;
-  if(distance<detectionRange){
-    //detected
-    //target=servoPos;
-//    Serial.println("found");
-    //tracked=true;
-    if(trackCount<trackCountMax)
-      trackCount+=1;
-    if(servoPos>servoPosMin){
-      servoPos--;
-      myServo.write(servoPos);
-    }
-    
-    
+  int distanceL=getValAvg(pingL);
+  int distanceR=getValAvg(pingR);
+  /*
+  Serial.print(distanceL);
+  Serial.print(" ");
+  Serial.println(distanceR);
+  */
+  boolean trackedL=distanceL<detectionRange?true:false;
+  boolean trackedR=distanceR<detectionRange?true:false;
+  
+  if(trackedL||trackedR){
+    if(trackCount<trackCountMax) trackCount++;
   }else{
-    //not detected
-    //target=90;
-    if(trackCount>0)
-      trackCount--;
-      
-    if(servoPos<servoPosMax){
-      servoPos++;
-      myServo.write(servoPos);
-
-    }else{
-      tracked=false;
-      servoPos=servoPosMin;
-      myServo.write(servoPos);
-
-      delay(20);
-    }
+    if(trackCount>0) trackCount--;
   }
+  
+  target=90;
+  if(trackedL)target-=90;
+  if(trackedR)target+=90;
 }
-int getValAvg(){
+int getValAvg(int sensorPin){
   int sum=0;
   for(int i=0;i<valSample_len;i++){
-    sum+=getDistance();
+    sum+=getDistance(sensorPin);
   }
   int valAvg=sum/valSample_len;
   return valAvg;
 }
-int getDistance(){
-  int val=abs((6762/(Robot.analogRead(M0)-9))-4);
-  return val;
+int getDistance(int sensorPin){
+  pinMode(sensorPin,OUTPUT);
+  digitalWrite(sensorPin,LOW);
+  delayMicroseconds(2);
+  digitalWrite(sensorPin, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(sensorPin, LOW);
+  
+  pinMode(sensorPin, INPUT);
+  int duration = pulseIn(sensorPin, HIGH, 11800);
+  return getCMs(duration);
+}
+long getCMs(int ms){
+  if(ms!=0){
+    return ms/29/2;
+  }else{
+    return 1000;
+  }
+}
+
+boolean onEdge(){
+  for(int i=0;i<5;i++){
+    if(robot.getIRArray(i)<edgeValue)
+      return true;
+  }
+  return false;
 }
 
 void searchMode(){
-  Robot.motorsWrite(60,-60);
+  robot.go(searchSpeed,-searchSpeed);
 }
-
 void trackMode(){
   error=target-90;
-  //Serial.println(error);
-  int res=error*0.3;
+  int res=error*trackOffset;
   if(res<0){
-    res-=50;
+    res-=trackSpeed;
   }else if(res>0){
-    res+=50;
+    res+=trackSpeed;
   }
-  //if(res==0){
-  //  Robot.motorsStop();
-  //}else{
-  Robot.motorsWrite(res,-res);
-  //}
-  //Robot.motorsWrite(res+80,80-res);
-  
+  robot.go(res,-res);
 }
 void attackMode(){
   error=target-90;
-  //Serial.println(error);
-  int res=error*0.5;
+  int res=error*attackOffset;
   
-  Robot.motorsWrite(res+80,80-res);
+  robot.go(res+attackSpeed,attackSpeed-res);
+}
+void surviveMode(){
+  robot.go(-surviveSpeed,-surviveSpeed);
+  delay(500);
 }
