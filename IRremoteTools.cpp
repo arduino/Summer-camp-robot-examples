febd71044eaed0d936f4faec324b8f0cb5f5f80d
Xun Yang <x.yang@arduino.cc>
2014-06-17 12:52:30 +0200

    Squashed 'TinkerKitIRremote/' changes from 0b900e0..d77bced
    d77bced Fixed problem with taking address of temporary [-fpermissive]
    af1e564 IRremote now compatible with the wrong TK IRs
    
    git-subtree-dir: TinkerKitIRremote
    git-subtree-split: d77bced5dd0ae84933b44fc41f31139450be542d

--- IRremoteTools.cpp
+++ IRremoteTools.cpp
@@ -2,20 +2,20 @@
 #include "IRremoteTools.h"
 #include <Arduino.h>
 
-IRrecv * irrecv=(IRrecv*)malloc(sizeof(IRrecv));
+IRrecv irrecv=IRrecv();
 decode_results results; // container for received IR codes
 
 void beginIRremote(int recvpin){
-	irrecv=&IRrecv(recvpin);
-	irrecv->enableIRIn(); // Start the receiver
+	irrecv.setRecvPin(recvpin);
+	irrecv.enableIRIn(); // Start the receiver
 }
 
 bool IRreceived(){
-	return irrecv->decode(&results);
+	return irrecv.decode(&results);
 }
 
 void resumeIRremote(){
-    irrecv->resume(); // resume receiver
+    irrecv.resume(); // resume receiver
 }
 
 unsigned long getIRresult(){
